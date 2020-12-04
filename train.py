a2ca85af224be0a74747b3fb508cecb29df2e51d
franke-g1 <fguarino71@gmail.com>
2020-12-04 15:35:05 -0500

    Finally! To have a successful AI, it requires atleast a good 1 minute run. I used velocity 15 and 14 as training data, and I gave it pretty shoddy training data, but it can last a few volleys. Next I will revise the training data and possibly change the classifier to SVC possibly.

---
+++ train.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from joblib import dump, load
+
+
+data = pd.read_csv('pong_data.csv')
+train_x = data[['ball_x', 'ball_y']]
+print(train_x.shape)
+train_y = data['paddle_y']
+print(train_y.shape)
+
+model = GaussianNB()
+model.fit(train_x, train_y)
+dump(model, 'model.joblib')
+
+
+
+
+
+
